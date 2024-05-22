458a8928b2fc9b1f77106b9a980e0264f53f5cd7
wuhongyi <wuhongyi@qq.com>
2024-05-22 23:16:33 +0800

    20240522 update firmware

--- DecodeAndSortAll/UserDefine.hh
+++ DecodeAndSortAll/UserDefine.hh
@@ -4,9 +4,9 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 日 2月 18 01:13:12 2024 (+0800)
-// Last-Updated: 五 2月 23 16:42:13 2024 (+0800)
+// Last-Updated: 日 5月 12 16:57:42 2024 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 3
+//     Update #: 4
 // URL: http://wuhongyi.cn 
 
 #ifndef _USERDEFINE_H_
@@ -29,9 +29,9 @@
 
 #define RAWFILEPATH "/home/wuhongyi/"   //Path to the original binary
 #define RAWFILENAME "data"                   //The file name of the original file
-#define MODNUMBER 3   //Number of modules used in this crate
-const unsigned short SamplingRate[MODNUMBER] = {125, 125, 125};//Specify the sampling rate of each modules separately; 100/250/500 sampling rates; 0 to skip the module
-const unsigned short Firmware[MODNUMBER] = {0, 0, 2};//DPP_PHA=0 DPP_ZLE=1 DPP_PSD=2 DPP_DAW=3 OPEN=4 Scope=5
+#define MODNUMBER 1   //Number of modules used in this crate
+const unsigned short SamplingRate[MODNUMBER] = {500};//Specify the sampling rate of each modules separately; 100/250/500 sampling rates; 0 to skip the module
+const unsigned short Firmware[MODNUMBER] = {2};//DPP_PHA=0 DPP_ZLE=1 DPP_PSD=2 DPP_DAW=3 OPEN=4 Scope=5
 
 
 
