33f69a540f6ccc50fa9a1e55d4a9921d418cfcc8
wuhongyi <wuhongyi@qq.com>
2024-11-12 00:40:03 +0800

    20241112

--- README/zh/source/conf.py
+++ README/zh/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Hongyi Wu(吴鸿毅)'
 author = 'Hongyi Wu(吴鸿毅)'
 
 # The full version, including alpha/beta/rc tags
-release = 'V20241025'
+release = 'V20241112'
 
 
 # -- General configuration ---------------------------------------------------
